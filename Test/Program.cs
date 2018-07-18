4e39e3c204b43e4a506142bca6e81173136368a3
David Slutsky <d6000d@gmail.com>
2018-07-18 17:49:56 +0300

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -11,7 +11,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            AKI.Client client = AKI.Client.StartGame(AKI.Client.Language.Portuguese);
+            AKI.Client client = AKI.Client.StartGame(AKI.Client.Language.Russian);
             bool GameFinished = false;
             List<ulong> DeclinedCharacters = new List<ulong>();
             while (!GameFinished)
